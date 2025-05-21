206a4bf0b5cd27e05b2f6b78b62d0535a28b173c
Novalunosis17 <mariano.rhumer92605@gmail.com>
2025-05-21 20:15:02 +0800

    admin changes only

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from './logo.png'
+import Rlogo from './Rlogo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
@@ -8,6 +9,7 @@ import parcel_icon from './parcel_icon.png'
 
 export const assets ={
     logo,
+    Rlogo,
     add_icon,
     order_icon,
     profile_image,
